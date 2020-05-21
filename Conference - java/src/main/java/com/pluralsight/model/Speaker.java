185a2873c7d49bde2601a296cb59f732e3417fe6
amboraes <amboraes@hotmail.com>
2020-05-21 17:56:17 -0500

    Adding courses

---
+++ Conference - java/src/main/java/com/pluralsight/model/Speaker.java
@@ -0,0 +1,32 @@
+package com.pluralsight.model;
+
+public class Speaker {
+
+    private String firstname;
+    private String lastname;
+    private double seedNum;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public double getSeedNum() {
+        return seedNum;
+    }
+
+    public void setSeedNum(double seedNum) {
+        this.seedNum = seedNum;
+    }
+}
